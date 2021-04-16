43f2b17d12121390980cdcbcf479ee785f136639
Ammar Faizi <ammarfaizi2@gmail.com>
2021-04-16 18:28:43 +0700

    Add license comment
    Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

--- src/bluetea/lib/getopt.c
+++ src/bluetea/lib/getopt.c
@@ -1,3 +1,11 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ *  src/bluetea/lib/getopt.c
+ *
+ *  Getopt library
+ *
+ *  Copyright (C) 2021  Ammar Faizi
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
